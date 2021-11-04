3b840873ead66c3ecd07261ac58c2c38034ca197
spb71 <shem.baijoo@gmail.com>
2021-11-04 10:02:18 -0400

    Updated environment variables

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -31,7 +31,7 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -137,7 +137,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
